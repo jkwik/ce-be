912e4768335459cb2faad10a6fd2fd938d128a0e
SolomonFett <38338715+SolomonFett@users.noreply.github.com>
2020-03-22 13:41:15 -0500

    approveClient route done but needs to be checked by Justin

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy, inspect
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -13,20 +13,22 @@ CORS(app, supports_credentials=True)
 
 # Set the database uri to the config var and create a connection to the db. In a real production app
 # this connection string would be set as an environment variable
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv
+
+                        # app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://nvjgfrayuptlkt:e7d1e4aeddb1b44888b9817872f22fc522d332bb0d453b1633bbf8688f57d266@ec2-18-235-20-228.compute-1.amazonaws.com:5432/d784oou6iuu63p"
 # app.config["SECRET_KEY"] = 'S3CRET!K3Y11!'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import User, user_schema
 
-@app.route("/health", methods=["GET"])
+@app.route("/health", methods=['GET'])
 def health():
     return {
         "Success": True
     }
 
-@app.route("/signUp", methods=["POST"])
+@app.route("/signUp", methods=['POST'])
 def signUp():
     body = request.get_json(force=True)
     user = User()
@@ -52,3 +54,36 @@ def signUp():
     return {
         "user": result
     }
+
+@app.route('/approveClient', methods=['PUT'])
+def approveClient():
+    body = request.get_json(force=True)
+    user = User()
+    # retrieve user with id passed in
+    user = User.query.get(body['id'])
+
+    if body['access_token'] == user.access_token :
+        try:
+            # update the approved field for this user
+            user.approved = True
+            db.session.commit()
+        except:
+            return {
+                "error": "could not approve client for this user"
+            }
+    else:
+        return {
+                "error": "incorrect access token"
+        }
+    
+    # Refresh the user to grab the id
+    db.session.refresh(user)
+
+    # Grab the user from the database and dump the result into a user schema
+    user = User.query.get(user.id)
+    result = user_schema.dump(user)
+    # Return the user
+    return {
+        "user": result
+    }
+
