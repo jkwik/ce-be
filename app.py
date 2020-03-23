4196536411f69103cedce0319f614cefb01e7ee8
Justin Kwik <kwik@wisc.edu>
2020-03-23 09:10:21 -0500

    added email format validation encrypted password

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy, inspect
 from flask_cors import CORS
 from dotenv import load_dotenv
 from sqlalchemy.exc import IntegrityError
+from email_validator import validate_email, EmailNotValidError
+from flask_bcrypt import Bcrypt
 import os
 
 load_dotenv()
@@ -18,6 +20,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = 'S3CRET!K3Y11!'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from models import User, user_schema
 
@@ -30,17 +33,36 @@ def health():
 @app.route("/signUp", methods=["POST"])
 def signUp():
     body = request.get_json(force=True)
-    user = User()
 
-    user = User(first_name=body['first_name'], last_name=body['last_name'], email=body['email'], password=body['password'])
+    # Validate that the email is the correct format
+    try:
+        v = validate_email(body['email']) # validate and get info
+        body['email'] = v["email"] # replace with normalized form
+    except EmailNotValidError as e:
+        # email is not valid, return error code
+        return {
+            "error": "Invalid Email Format"
+        }, 406
+
+    # Encrypt the password
+    encodedPassword = bcrypt.generate_password_hash(body['password'])
+
+    # Create the user with the encoded password
+    user = User()
+    user = User(first_name=body['first_name'], last_name=body['last_name'], email=body['email'], password=encodedPassword)
 
     try:
         db.session.add(user)
         db.session.commit()
     except IntegrityError:
         return {
-            "error": "403"
-        }
+            "error": "Duplicate Email"
+        }, 409
+    except Exception as e:
+        return {
+            "error": "Internal Server Error"
+        }, 500
+        raise
 
     # Refresh the user to grab the id
     db.session.refresh(user)
@@ -48,6 +70,9 @@ def signUp():
     # Grab the user from the database and dumpp the result into a user schema
     user = User.query.get(user.id)
     result = user_schema.dump(user)
+
+    # Delete the password from the response, we don't want to transfer this over the wire
+    del result['password']
 
     # Return the user
     return {
