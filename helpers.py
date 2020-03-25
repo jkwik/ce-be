663d5c35af6652ff85b38c8b7564bc3190adfa7d
SolomonFett <38338715+SolomonFett@users.noreply.github.com>
2020-03-25 14:58:27 -0500

    Update helpers.py
    Co-Authored-By: Justin Ingemar Kwik <kwik@wisc.edu>

--- helpers.py
+++ helpers.py
@@ -32,7 +32,7 @@ def sendApprovedEmail(mail, to, first_name, last_name):
         callback = os.getenv("FRONTEND_URL")
     else:
         callback = os.getenv("DEV_FRONTEND_URL")
-    callback = callback + 'auth/login'
+    callback = callback + 'login'
 
     try:
         msg = Message(
