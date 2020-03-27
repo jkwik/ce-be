a36e478f2575dcad961c946a3fb1b387bff11482
Kevin <kkemp3@wisc.edu>
2020-03-26 20:22:44 -0500

    Test skeleton for endpoints

--- test_endpoints.py
+++ test_endpoints.py
@@ -3,6 +3,7 @@ import tempfile
 
 import pytest
 from app import app
+from models import User, UserSchema, Role
 
 
 def test_empty_db(client):
@@ -14,3 +15,34 @@ def test_health(client):
     rv = client.get('/health')
 
     assert b''
+
+def test_signup(client):
+    assert True
+
+def test_approve_client(client):
+    assert True
+
+def test_client_list(client):
+    assert True
+
+def test_update_profile(client):
+    assert True
+
+def test_verify_user(client):
+    assert True
+
+def test_login(client):
+    assert True
+
+def test_logout(client):
+    assert True
+
+def test_forgot_password(client):
+    assert True
+
+def test_reset_password(client):
+    assert True
+
+def test_terminate_client(client):
+    assert True
+
