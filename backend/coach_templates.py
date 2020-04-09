c019681c2fa57d74e8f067045a486afc5e7458fe
SolomonFett <38338715+SolomonFett@users.noreply.github.com>
2020-04-09 14:48:49 -0500

    finished get /coach/template, get coach/templates, working on get coach/session

--- backend/coach_templates.py
+++ backend/coach_templates.py
@@ -1,65 +1,125 @@
-from backend import app
-from backend.models.coach_templates import CoachTemplate, coach_session_schemas, Exercise, coach_exercise_schema, CoachSession, coach_exercise_schemas
+from backend import app, db
+from backend.middleware.middleware import http_guard
+from flask import request, session
+from sqlalchemy.exc import IntegrityError
+# from sqlalchemy.orm import defer, joinedload, load_only, subqueryload
+from backend.models.user import Role
+from backend.models.coach_templates import CoachTemplate, coach_template_schema, coach_template_schemas, coach_session_schema, coach_session_schemas, Exercise, coach_exercise_schema, CoachSession, coach_exercise_schemas, CoachExercise, Exercise
 
-# Iteration 2 -- RELATIONSHIP TESTS
-
-# Coach_sessions test (DOES NOT WORK  COMPLETELY)
-@app.route("/coachSession", methods=['GET'])
-def coachSession():
-    # retrieve sessions belonging to the template with the passed in template id
-    coach_template = CoachTemplate()
-    coach_template = CoachTemplate.query.filter_by(id='1').first()
-    # retrieve using foreign key "coach_sessions"
-    sessions = coach_template.coach_sessions
-
-    if sessions == None:
+# Iteration 2
+# Return a list of templates the coach has created from the Templates table
+@app.route("/coach/templates", methods=['GET'])
+@http_guard(renew=True, nullable=False)
+def coachTemplates(token_claims):
+    # check that the user's role is COACH
+    if token_claims['role'] != Role.COACH.name:
         return {
-            "error": "Invalid session id"
-        }, 404
+            "error": "Expected role of COACH"
+    }, 400
 
-    result = coach_session_schemas.dump(sessions)
+    templates = CoachTemplate.query.all()
+
+    if templates == None:
+        return {
+            "error": "There are no Coach_templates"
+        }
+
+    result = coach_template_schemas.dump(templates)
 
     return {
-        "coach sessions": result
+        "result": result
     }
 
-
-# Coach_exercises test (DOES NOT WORK  COMPLETELY)
-@app.route("/coachExercise", methods=['GET'])
-def coachExercise():
-    # retrieve coach_exercise belonging to the exercise with the passed in exercise id
-    exercise = Exercise()
-    exercise = Exercise.query.filter_by(id='1').first()
-    # retrieve using foreign key "coach_exercises"
-    coach_exercise = exercise.coach_exercises
-
-    if coach_exercise == None:
+# Return the coach_template from coach_template_id passed in
+@app.route("/coach/template", methods=['GET'])
+@http_guard(renew=True, nullable=False)
+def coachTemplate(token_claims):
+    # check that the user's role is COACH
+    if token_claims['role'] != Role.COACH.name:
         return {
-            "error": "Invalid session id"
+            "error": "Expected role of COACH"
+    }, 400
+
+    id = request.args.get('coach_template_id')
+    
+    template = CoachTemplate.query.filter_by(id=id).first()
+
+    if template == None:
+        return {
+            "error": "Coach_template not found with given id: " + id
+        }
+
+    result = coach_template_schema.dump(template)
+    
+    return result
+
+
+# Return the coach_session from the coach_session_id passed in
+@app.route("/coach/session", methods=['GET'])
+@http_guard(renew=True, nullable=False)
+def coachSession(token_claims):
+    # check that the user's role is COACH
+    if token_claims['role'] != Role.COACH.name:
+        return {
+            "error": "Expected role of COACH"
+    }, 400
+
+    id = request.args.get('coach_session_id')
+    
+    session = CoachSession.query.filter_by(id=id).first()
+    # exercises = CoachExercise.query.join(Exercise, CoachExercise.exercise_id==Exercise.id)
+
+    if session == None:
+        return {
+            "error": "Coach_session not found with given id: " + id
+        }
+
+    result = coach_session_schema.dump(session)
+    
+    return result
+
+
+# Return the coach_exercise from the coach_exercise_id passed in
+@app.route("/coach/exercise", methods=['GET'])
+@http_guard(renew=True, nullable=False)
+def coachExercise(token_claims):
+    # check that the user's role is COACH
+    if token_claims['role'] != Role.COACH.name:
+        return {
+            "error": "Expected role of COACH"
+    }, 400
+
+    id = request.args.get('coach_exercise_id')
+
+    exercise = CoachExercise.query.filter_by(id=id).first()
+
+    if exercise == None:
+        return {
+            "error": "Coach_exercise not found with given id: " + id
         }, 404
 
-    result = coach_exercise_schema.dump(coach_exercise)
+    result = coach_exercise_schema.dump(exercise)
 
-    return {
-        "coach_exercise": result
-    }
+    return result
 
-# Coach_exercises to coach_sessions (DOES NOT WORK  COMPLETELY)
-@app.route("/ce_to_cs", methods=['GET'])
-def ce_to_cs():
-    # retrieve exercises belonging to the session with the passed in session id
-    coach_session = CoachSession()
-    coach_session = CoachSession.query.filter_by(id='1').first()
-    # retrieve using foreign key "coach_exercises"
-    exercises = coach_session.coach_exercises
 
-    if exercises == None:
-        return {
-            "error": "Invalid session id"
-        }, 404
 
-    result = coach_exercise_schemas.dump(exercises)
+# # Coach_exercises to coach_sessions (DOES NOT WORK  COMPLETELY)
+# @app.route("/ce_to_cs", methods=['GET'])
+# def ce_to_cs():
+#     # retrieve exercises belonging to the session with the passed in session id
+#     coach_session = CoachSession()
+#     coach_session = CoachSession.query.filter_by(id='1').first()
+#     # retrieve using foreign key "coach_exercises"
+#     exercises = coach_session.coach_exercises
 
-    return {
-        "Coach_exercises ": result
-    }
+#     if exercises == None:
+#         return {
+#             "error": "Invalid session id"
+#         }, 404
+
+#     result = coach_exercise_schemas.dump(exercises)
+
+#     return {
+#         "Coach_exercises ": result
+#     }
